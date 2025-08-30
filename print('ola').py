135a2606d3c139c70f09457ae4c3b77394ffc7fe
LUIZ GUSTAVO LEANDRO SOARES <luiz.leandro@aluno.senac.rio>
2025-08-29 21:50:57 -0300

    oi

--- print('ola').py
+++ print('ola').py
@@ -1,1 +1,16 @@
-print('ola')
+# um morador deseja saber quantas lampadas sao necessarias
+# p iluminar um comodo de sua casa, fazer um programa que receba
+# potencia da lampada em watts
+# largura e comprimento do comodo em metros 
+# potencia ideal de iluminacao e de 3 watts por metro quadrado
+# cada 3 m² deve haver um bocal p lampada
+# calcular area do comodo 
+# potencia total necessaria
+# qtdd de lampadas necessarias arredondando pra confirmacao
+while True:  
+    try: 
+        potencia_lampada = input('qual potencia da lampada? ')
+        break 
+    except: 
+          print('ola')
+          
