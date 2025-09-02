a1880ca4d884471e6603c038d5cc43cfafc61089
luizgust2234 <luizgust2234@gmail.com>
2025-09-02 20:37:12 -0300

    Add files via upload

--- atv4.py
+++ atv4.py
@@ -1,3 +1,20 @@
 #atv_aula4
 
-produtos = int(input('quantos produtos deseja cadastrar? '))
+qtdd = int(input('quantos produtos deseja cadastrar? '))
+print('digite o nome e o valor de cada produto! ')
+ 
+nomes = []
+valores = []
+
+for i in range(qtdd):
+    nome = input(f'digite o nome do {i+1} produto: ')
+    valor = float(input(f'digite o valor do {i+1} produto: R$ ')) 
+    nomes.append(nome)
+    valores.append(valor)
+
+    print('\n===== relatorio de produtos =====')
+    for i in range(qtdd):
+        print(f'{i+1}. {nomes[i]} - R$ {valores[i]:}')
+        
+
+
